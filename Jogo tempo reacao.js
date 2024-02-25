00c062ec2e570528b7fe4cbc249ecaa8c74679a8
Felipe Sena <felipedosssena@gmail.com>
2024-02-25 14:06:57 -0300

    Formas aparecem novamente depois de um tempo

--- Jogo tempo reacao.js
+++ Jogo tempo reacao.js
@@ -8,14 +8,31 @@ var triangulo = document.getElementById("triangulo");
 quadrado.addEventListener("click", function(){
 
     quadrado.remove();
+    setTimeout( function(){
+
+        tela.appendChild(quadrado);
+    }, 5000);
 });
 
 circulo.addEventListener("click", function(){
 
     circulo.remove();
-})
+    setTimeout( function(){
+
+        tela.appendChild(circulo);
+    }, 5000);
+});
 
 triangulo.addEventListener("click", function(){
 
     triangulo.remove();
-})
+    setTimeout( function(){
+
+        tela.appendChild(triangulo);
+    }, 5000);
+});
+
+/*setTimeout( function(){
+
+    document.body.appendChild(quadrado);
+}, 5000);*/
