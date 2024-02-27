ece31747f7e9e7cc00e491cfc128f99ad722757e
Felipe Sena <felipedosssena@gmail.com>
2024-02-27 11:30:44 -0300

    Forma muda a cor e a posição apos click

--- Jogo tempo reacao.js
+++ Jogo tempo reacao.js
@@ -3,36 +3,70 @@ var quadrado = document.getElementById("quadrado");
 var circulo = document.getElementById("circulo");
 var triangulo = document.getElementById("triangulo");
 
+var limiteMaximoTop = 84;
+var limiteMinimoTop = 20;
+var limiteMaximoLeft = 91;
+var limiteMinimoLeft = 1;
 
+let novaPosicaoTop = 0;
+let novaPosicaoLeft = 0;
+let corAleatoria = 0;
+let formaAleatoria = 0;
 
-quadrado.addEventListener("click", function(){
+           
+            quadrado.addEventListener("click", function(){
+            
+                quadrado.remove();
 
-    quadrado.remove();
-    setTimeout( function(){
+                setTimeout( function(){
+                
+                    novaPosicaoTop = Math.floor(Math.random() * (limiteMinimoTop - limiteMaximoTop +1)) + limiteMaximoTop;
+                    novaPosicaoLeft = Math.floor(Math.random() * (limiteMinimoLeft - limiteMaximoLeft +1)) + limiteMaximoLeft;
 
-        tela.appendChild(quadrado);
-    }, 5000);
-});
+                    quadrado.style.position = "absolute";
+                    quadrado.style.top = novaPosicaoTop + "%";
+                    quadrado.style.left = novaPosicaoLeft + "%";
 
-circulo.addEventListener("click", function(){
+                    corAleatoria = '#'+Math.floor(Math.random()*16777215).toString(16);
+                    quadrado.style.backgroundColor = corAleatoria;
+                    tela.appendChild(quadrado);
+                }, 1000);
+            });
 
-    circulo.remove();
-    setTimeout( function(){
+            circulo.addEventListener("click", function(){
 
-        tela.appendChild(circulo);
-    }, 5000);
-});
+                circulo.remove();
 
-triangulo.addEventListener("click", function(){
+                setTimeout(function() {
+                    novaPosicaoTop = Math.floor(Math.random() * (limiteMinimoTop - limiteMaximoTop +1)) + limiteMaximoTop;
+                    novaPosicaoLeft = Math.floor(Math.random() * (limiteMinimoLeft - limiteMaximoLeft +1)) + limiteMaximoLeft;
 
-    triangulo.remove();
-    setTimeout( function(){
+                    circulo.style.position = "absolute";
+                    circulo.style.top = novaPosicaoTop + "%";
+                    circulo.style.left = novaPosicaoLeft +"%";
 
-        tela.appendChild(triangulo);
-    }, 5000);
-});
+                    corAleatoria = '#'+Math.floor(Math.random()*16777215).toString(16);
+                    circulo.style.backgroundColor = corAleatoria;
 
-/*setTimeout( function(){
+                    tela.appendChild(circulo);
+                },1000);
+            });
 
-    document.body.appendChild(quadrado);
-}, 5000);*/
+            triangulo.addEventListener("click", function(){
+
+                triangulo.remove();
+                setTimeout(function() {
+
+                    novaPosicaoTop = Math.floor(Math.random() * (limiteMinimoTop - limiteMaximoTop +1)) + limiteMaximoTop;
+                    novaPosicaoLeft = Math.floor(Math.random() * (limiteMinimoLeft - limiteMaximoLeft +1)) + limiteMaximoLeft;
+
+                    triangulo.style.position = "absolute";
+                    triangulo.style.top = novaPosicaoTop + "%";
+                    triangulo.style.left = novaPosicaoLeft +"%";
+
+                    corAleatoria = '#'+Math.floor(Math.random()*16777215).toString(16);
+                    triangulo.style.borderBottomColor = corAleatoria;
+
+                    tela.appendChild(triangulo);
+                }, 1000);
+            });
