202eaab16f11edaa31af3661404a516d4975677a
Felipe Sena <felipedosssena@gmail.com>
2024-02-28 15:28:14 -0300

    Apos o click é uma forma aleatoria que aparece

--- Jogo tempo reacao.js
+++ Jogo tempo reacao.js
@@ -13,60 +13,179 @@ let novaPosicaoLeft = 0;
 let corAleatoria = 0;
 let formaAleatoria = 0;
 
-           
-            quadrado.addEventListener("click", function(){
-            
-                quadrado.remove();
+let num_1a3;
 
-                setTimeout( function(){
+    circulo.remove();
+    triangulo.remove();
+
+
                 
-                    novaPosicaoTop = Math.floor(Math.random() * (limiteMinimoTop - limiteMaximoTop +1)) + limiteMaximoTop;
-                    novaPosicaoLeft = Math.floor(Math.random() * (limiteMinimoLeft - limiteMaximoLeft +1)) + limiteMaximoLeft;
+                quadrado.onclick = function(){
 
-                    quadrado.style.position = "absolute";
-                    quadrado.style.top = novaPosicaoTop + "%";
-                    quadrado.style.left = novaPosicaoLeft + "%";
+                    quadrado.remove();
+                
+                setTimeout ( function(){
 
                     corAleatoria = '#'+Math.floor(Math.random()*16777215).toString(16);
-                    quadrado.style.backgroundColor = corAleatoria;
-                    tela.appendChild(quadrado);
+                    novaPosicaoTop = Math.floor(Math.random() * (limiteMinimoTop - limiteMaximoTop +1)) + limiteMaximoTop;
+                    novaPosicaoLeft = Math.floor(Math.random() * (limiteMinimoLeft - limiteMaximoLeft +1)) + limiteMaximoLeft;
+                
+                    num_1a3 = Math.floor(Math.random() * 3) + 1;
+
+                   if(num_1a3 == 1){
+
+                        quadrado.style.position = "absolute";
+                        quadrado.style.top = novaPosicaoTop + "%";
+                        quadrado.style.left = novaPosicaoLeft + "%";
+
+                        quadrado.style.backgroundColor = corAleatoria;
+
+                        tela.appendChild(quadrado);
+
+                    }
+
+                    else if(num_1a3 == 2){
+
+                        circulo.style.position = "absolute";
+                        circulo.style.top = novaPosicaoTop + "%";
+                        circulo.style.left = novaPosicaoLeft +"%";
+
+                        circulo.style.backgroundColor = corAleatoria;
+
+                        tela.appendChild(circulo);
+                    }
+
+                    else if(num_1a3 == 3){
+
+                        triangulo.style.position = "absolute";
+                        triangulo.style.top = novaPosicaoTop + "%";
+                        triangulo.style.left = novaPosicaoLeft +"%";
+
+                        triangulo.style.borderBottomColor = corAleatoria;
+
+                        tela.appendChild(triangulo);
+                    }
+
                 }, 1000);
-            });
+                };
 
-            circulo.addEventListener("click", function(){
 
-                circulo.remove();
 
-                setTimeout(function() {
-                    novaPosicaoTop = Math.floor(Math.random() * (limiteMinimoTop - limiteMaximoTop +1)) + limiteMaximoTop;
-                    novaPosicaoLeft = Math.floor(Math.random() * (limiteMinimoLeft - limiteMaximoLeft +1)) + limiteMaximoLeft;
 
-                    circulo.style.position = "absolute";
-                    circulo.style.top = novaPosicaoTop + "%";
-                    circulo.style.left = novaPosicaoLeft +"%";
 
-                    corAleatoria = '#'+Math.floor(Math.random()*16777215).toString(16);
-                    circulo.style.backgroundColor = corAleatoria;
 
-                    tela.appendChild(circulo);
-                },1000);
-            });
 
-            triangulo.addEventListener("click", function(){
 
-                triangulo.remove();
-                setTimeout(function() {
 
-                    novaPosicaoTop = Math.floor(Math.random() * (limiteMinimoTop - limiteMaximoTop +1)) + limiteMaximoTop;
-                    novaPosicaoLeft = Math.floor(Math.random() * (limiteMinimoLeft - limiteMaximoLeft +1)) + limiteMaximoLeft;
 
-                    triangulo.style.position = "absolute";
-                    triangulo.style.top = novaPosicaoTop + "%";
-                    triangulo.style.left = novaPosicaoLeft +"%";
+
+
+
+
+                triangulo.onclick = function(){
+
+                    triangulo.remove();
+
+                setTimeout ( function(){
 
                     corAleatoria = '#'+Math.floor(Math.random()*16777215).toString(16);
-                    triangulo.style.borderBottomColor = corAleatoria;
+                    novaPosicaoTop = Math.floor(Math.random() * (limiteMinimoTop - limiteMaximoTop +1)) + limiteMaximoTop;
+                    novaPosicaoLeft = Math.floor(Math.random() * (limiteMinimoLeft - limiteMaximoLeft +1)) + limiteMaximoLeft;
+                
+                    num_1a3 = Math.floor(Math.random() * 3) + 1;
 
-                    tela.appendChild(triangulo);
+                   if(num_1a3 == 1){
+
+                        quadrado.style.position = "absolute";
+                        quadrado.style.top = novaPosicaoTop + "%";
+                        quadrado.style.left = novaPosicaoLeft + "%";
+
+                        quadrado.style.backgroundColor = corAleatoria;
+
+                        tela.appendChild(quadrado);
+
+                    }
+
+                    else if(num_1a3 == 2){
+
+                        circulo.style.position = "absolute";
+                        circulo.style.top = novaPosicaoTop + "%";
+                        circulo.style.left = novaPosicaoLeft +"%";
+
+                        circulo.style.backgroundColor = corAleatoria;
+
+                        tela.appendChild(circulo);
+                    }
+
+                    else if(num_1a3 == 3){
+
+                        triangulo.style.position = "absolute";
+                        triangulo.style.top = novaPosicaoTop + "%";
+                        triangulo.style.left = novaPosicaoLeft +"%";
+
+                        triangulo.style.borderBottomColor = corAleatoria;
+
+                        tela.appendChild(triangulo);
+                    }
+
                 }, 1000);
-            });
+                };
+
+
+
+
+
+
+
+
+
+
+
+                circulo.onclick = function(){
+
+                    circulo.remove();
+
+                setTimeout ( function(){
+
+                    corAleatoria = '#'+Math.floor(Math.random()*16777215).toString(16);
+                    novaPosicaoTop = Math.floor(Math.random() * (limiteMinimoTop - limiteMaximoTop +1)) + limiteMaximoTop;
+                    novaPosicaoLeft = Math.floor(Math.random() * (limiteMinimoLeft - limiteMaximoLeft +1)) + limiteMaximoLeft;
+                
+                    num_1a3 = Math.floor(Math.random() * 3) + 1;
+
+                   if(num_1a3 == 1){
+
+                        quadrado.style.position = "absolute";
+                        quadrado.style.top = novaPosicaoTop + "%";
+                        quadrado.style.left = novaPosicaoLeft + "%";
+
+                        quadrado.style.backgroundColor = corAleatoria;
+
+                        tela.appendChild(quadrado);
+
+                    }
+
+                    else if(num_1a3 == 2){
+
+                        circulo.style.position = "absolute";
+                        circulo.style.top = novaPosicaoTop + "%";
+                        circulo.style.left = novaPosicaoLeft +"%";
+
+                        circulo.style.backgroundColor = corAleatoria;
+
+                        tela.appendChild(circulo);
+                    }
+
+                    else if(num_1a3 == 3){
+
+                        triangulo.style.position = "absolute";
+                        triangulo.style.top = novaPosicaoTop + "%";
+                        triangulo.style.left = novaPosicaoLeft +"%";
+
+                        triangulo.style.borderBottomColor = corAleatoria;
+
+                        tela.appendChild(triangulo);
+                    }
+
+                }, 1000);
+                };
